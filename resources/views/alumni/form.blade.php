8296aaca3f7ab58fad0a8ac8581065a601ab4bb0
yoga233 <rg.gamiiing233@gmail.com>
2025-06-05 10:18:04 +0700

    tes commit

--- resources/views/alumni/form.blade.php
+++ resources/views/alumni/form.blade.php
@@ -35,7 +35,7 @@
                     <p>Terima kasih atas partisipasi Anda dalam mengisi Tracer Study Lulusan ITATS</p>
                     <p class="mt-2">Tracer study ini hanya untuk program studi / jurusan yang masih aktif. Isi semua pertanyaan sesuai dengan kondisi Anda saat ini.</p>
                     <ul class="list-disc list-inside mt-2 space-y-1">
-                        <li>1. Mengevaluasi kualitas program pendidikan di ITATS.</li>
+                        <li>1. Mengevaluasi kualitas program pendidikan di ITATSpppppppppppp.</li>
                         <li>2. Memahami pengembangan karir lulusan.</li>
                         <li>3. Meningkatkan kualitas pendidikan dan layanan kemahasiswaan.</li>
                     </ul>
