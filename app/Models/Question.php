2c15eed6acee0b7fc64f3977189164796a9d934b
yoga233 <rg.gamiiing233@gmail.com>
2025-06-05 11:02:43 +0700

    tesssssddddss commit

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,45 +2,49 @@
 
 namespace App\Models;
 
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-
     use HasFactory;
-    protected $fillable = ['question_text', 'question_type_id', 'is_required', 'other_option'];
 
-    // untuk dropdown kategory
-    public const EMPLOYMENT_STATUSES = [
-        'Bekerja',
-        'Freelance',
-        'Wirausaha',
-        'Belum Bekerja',
-        'Studi Lanjut',
+    protected $fillable = [
+        'question_text',
+        'question_type_id',
+        'is_required',
+        'scale_labels',
     ];
 
+    protected $casts = [
+        'scale_labels' => 'array',
+    ];
+
+    public const EMPLOYMENT_STATUSES = [
+        'Bekerja (full time/part time)',
+        'Belum Memungkinkan Bekerja',
+        'Wiraswasta',
+        'Melanjutkan Pendidikan',
+        'Tidak Kerja Tetapi Sedang Mencari Kerja',
+    ];
 
     public function questiontype()
     {
         return $this->belongsTo(QuestionType::class, 'question_type_id');
     }
 
-    // public function answerDetails()
-    // {
-    //     return $this->hasMany(AnswerDetail::class);
-    // }
-
-    public function questionConditions(){
-        return $this->hasMany(QuestionCondition::class);
-    }
-
-    public function options(){
+    public function options()
+    {
         return $this->hasMany(Option::class);
     }
 
-    public function alumniAnswers(){
+    public function alumniAnswers()
+    {
         return $this->hasMany(AlumniAnswer::class, 'question_id');
+    }
+
+    public function questionConditions()
+    {
+        return $this->hasMany(QuestionCondition::class);
     }
 }
